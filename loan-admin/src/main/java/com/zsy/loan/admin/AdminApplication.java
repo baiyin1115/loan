a06b86e9a9e566c66d2e038dc6de38394a2acbb7
zhangbaiyin <baiyin_1115@163.com>
2019-01-20 21:44:48 +0800

    修改配置信息

--- loan-admin/src/main/java/com/zsy/loan/admin/AdminApplication.java
+++ loan-admin/src/main/java/com/zsy/loan/admin/AdminApplication.java
@@ -1,22 +1,13 @@
 package com.zsy.loan.admin;
 
-import com.zsy.loan.admin.config.properties.LoanProperties;
-import com.zsy.loan.admin.core.intercept.LogInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springboot.reload.plugin.annotation.EnableOnlineReload;
 
 /**
@@ -28,43 +19,12 @@ import springboot.reload.plugin.annotation.EnableOnlineReload;
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableCaching
-@ComponentScan(basePackages = {"com.zsy.loan","springboot.reload.plugin"})
+@ComponentScan(basePackages = {"com.zsy.loan", "springboot.reload.plugin"})
 @EntityScan(basePackages = "com.zsy.loan.bean.entity")
-@EnableOnlineReload(activeProfile= {"dev"},needAuth=true)
-public class AdminApplication extends WebMvcConfigurerAdapter {
+@EnableOnlineReload(activeProfile = {"dev"}, needAuth = true)
+public class AdminApplication {
 
   protected final static Logger logger = LoggerFactory.getLogger(AdminApplication.class);
-
-  @Autowired
-  LoanProperties loanProperties;
-
-  @Autowired
-  private LogInterceptor logInterceptor; // 实例化拦截器
-
-  /**
-   * 增加swagger的支持
-   */
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    if (loanProperties.getSwaggerOpen()) {
-      registry.addResourceHandler("swagger-ui.html")
-          .addResourceLocations("classpath:/META-INF/resources/");
-      registry.addResourceHandler("/webjars/**")
-          .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-  }
-
-  @Override
-  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
-  }
-
-  public void addInterceptors(InterceptorRegistry registry) {
-    // super.addInterceptors(registry);
-    // 注册自定义的拦截器passwordStateInterceptor
-    registry.addInterceptor(logInterceptor)
-        .addPathPatterns("/**"); //匹配要过滤的路径
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(AdminApplication.class, args);
--- loan-admin/src/main/java/com/zsy/loan/admin/config/web/WebConfig.java
+++ loan-admin/src/main/java/com/zsy/loan/admin/config/web/WebConfig.java
@@ -1,17 +1,25 @@
 package com.zsy.loan.admin.config.web;
 
+import com.zsy.loan.admin.config.properties.LoanProperties;
+import com.zsy.loan.admin.core.intercept.LogInterceptor;
 import com.zsy.loan.admin.core.xss.XssFilter;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.util.Config;
 import com.zsy.loan.service.web.listener.ConfigListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.context.request.RequestContextListener;
 
 import java.util.Arrays;
 import java.util.Properties;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * web 配置类
@@ -20,7 +28,7 @@ import java.util.Properties;
  * @date 2016年11月12日 下午5:03:32
  */
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
   /**
    * xssFilter注册
@@ -70,4 +78,36 @@ public class WebConfig {
     defaultKaptcha.setConfig(config);
     return defaultKaptcha;
   }
+
+  @Autowired
+  LoanProperties loanProperties;
+
+  @Autowired
+  LogInterceptor logInterceptor; // 实例化拦截器
+
+  /**
+   * 增加swagger的支持
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (loanProperties.getSwaggerOpen()) {
+      registry.addResourceHandler("swagger-ui.html")
+          .addResourceLocations("classpath:/META-INF/resources/");
+      registry.addResourceHandler("/webjars/**")
+          .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+  }
+
+  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+  }
+
+  public void addInterceptors(InterceptorRegistry registry) {
+    // super.addInterceptors(registry);
+    // 注册自定义的拦截器passwordStateInterceptor
+    registry.addInterceptor(logInterceptor)
+        .addPathPatterns("/**"); //匹配要过滤的路径
+  }
+
 }
